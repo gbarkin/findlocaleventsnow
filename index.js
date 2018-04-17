4228637d80b455a29b7d9ae30c9ab8919aeb5219
George Barkin <george.barkin@gmail.com>
2018-04-17 00:13:41 -0400

    added map and price comparison

--- index.js
+++ index.js
@@ -13,39 +13,46 @@ function getDataFromApi(searchTerm, callback) {
   // console.log(searchTerm);
 }
 
+function renderResult(result) {
+  if (result.priceRanges[0].min < myBudget.val()) {
+    return `
+<ul><li>Price: ${result.priceRanges[0].min} - ${result.name}</li></ul>
+  `;
+  } else {
+    console.log("yikes")
+  }
+}
+
 function displayTicketMasterData(data) {
   console.log(data);
-  var displayResults = $('.results');
-  data._embedded.events.forEach(function(item, index) {
-    let elem = $('.results-temp').children().clone();
-    console.log(item.name);
-    let title = (item.name);
-    $('.event-name').append(`${title}<br>`);
-  });
 
+  let results = data._embedded.events.map(function(item, index) {
+    return renderResult(item);
+
+  });
+  $('.js-results').html(results);
 }
+
 
 function watchSubmit() {
   $('.js-form').submit(event, function() {
     event.preventDefault();
     let queryCity = $(event.currentTarget).find('#city');
     let queryArtistName = $(event.currentTarget).find('#artist-name');
-    let queryMyBudget = $(event.currentTarget).find('#my-budget');
     let queryStartDate = $(event.currentTarget).find('#start-date').val().toString() + `T00:00:00Z`;
     let queryEndDate = $(event.currentTarget).find('#end-date').val().toString() + `T00:00:00Z`;
-
-    console.log(queryStartDate);
+    myBudget = $(event.currentTarget).find('#my-budget');
     $('.event-name').html('');
     let query = {
       keyword: queryArtistName.val(),
       city: queryCity.val(),
       startDateTime: queryStartDate,
       endDateTime: queryEndDate,
-      size: 20,
+      size: 10,
       page: 1
     };
     // findEventsInBudget(queryMyBudget);
-    console.log(query);
+    // console.log(query);
     getDataFromApi(query, displayTicketMasterData);
 
   });
