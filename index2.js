d064254e4f3c380d243560e0c428a6c7e9f58b60
George Barkin <george.barkin@gmail.com>
2018-04-27 07:45:21 -0400

    with youtube module

--- index2.js
+++ index2.js
@@ -2,12 +2,15 @@ const TicketMaster_URL = "https://app.ticketmaster.com/discovery/v2/events.json?
 
 const Youtube_URL = "https://www.googleapis.com/youtube/v3/search"
 
-
-
+//shrink inputs
+//add header to columns
+//make key/value bold
+// add labels for the form
 // form for submiting new event
 function watchSubmit() {
   $('.js-form').submit(event, function() {
     event.preventDefault();
+    $('.instructions').hide();
 
     // clear out previous data
     $('.event-name').html('');
@@ -16,8 +19,7 @@ function watchSubmit() {
     let queryCity = $('#city');
     let queryArtistName = $('#artist-name');
     let myBudget = $('#my-budget');
-    let myDate = $('#today-date');
-
+    let myDate = $('#today-date').val();
 
     // query for ticketmaster call
     // todo validate entry, try catch
@@ -26,8 +28,8 @@ function watchSubmit() {
       keyword: queryArtistName.val(),
       city: queryCity.val(),
       radius: 100,
-      startDateTime: `2018-04-26T00:00:00Z`,
-      size: 200,
+      startDateTime: `${myDate}T09:00:00Z`,
+      size: 100,
       page: 0
     };
     // getDataFrom Api ticketmaster
@@ -54,7 +56,6 @@ function getDataFromApi(searchTerm, callback) {
 // build ticket master data
 function buildTicketMasterData(data) {
   if (data._embedded) {
-    console.log(data._embedded);
     let results = data._embedded.events.map(function(item) {
       return renderResult(item);
     });
@@ -63,29 +64,42 @@ function buildTicketMasterData(data) {
 
   }
 }
-
+//clean data etc prior to moving data to template
 
 function renderResult(results) {
   let myBudget = $('#my-budget').val();
   if (results.priceRanges) {
     let eventPrice = Number(results.priceRanges[0].min);
-    console.log(eventPrice)
     if (eventPrice < myBudget) {
       return `
-    <h4 class="accordion-toggle">Event: ${results.name}. <span class="min-price">$${results.priceRanges[0].min}</span></h4>
+    <h2 class="accordion-toggle"> Date: ${results.dates.start.localDate}</span><span class="event-name">${results.name}.</span><span class="event-date"><span class="event-price"> $${results.priceRanges[0].min}</span></h2>
     <div class="accordion-content">
-      <p> Date: ${results.dates.start.localDate}</p>
+    <div class="event-date-time">
+      <p>  ${results.dates.start.localDate}</p>
       <p>Time:${results.dates.start.localTime}</p>
+      </div>
+      <div class="event-price-venue">
       <p>Price: $${results.priceRanges[0].min}</p>
+      <p>Venue: ${results._embedded.venues[0].name}</p>
+      </div>
+      <div class="vid-content"></div>
     </div>`;
-    } else {}
+    } else {
+      console.log("to be determined")
+    }
 
   } else {
     return `
-    <h4 class="accordion-toggle">Event: ${results.name}.</h4>
+  <h2 class="accordion-toggle"> Date: ${results.dates.start.localDate}</span><span class="event-name">${results.name}.</span><span class="event-date"><span class="event-price"> Price Unavailable</span></h2>
     <div class="accordion-content">
-      <p> Date: ${results.dates.start.localDate}</p>
+    <div class="event-date-time">
+      <p>  ${results.dates.start.localDate}</p>
       <p>Time:${results.dates.start.localTime}</p>
+      </div>
+      <div class="event-price-venue">
+      <p>Venue: ${results._embedded.venues[0].name}</p>
+      </div>
+      <div class="vid-content"></div>
     </div>`;
   }
 }
@@ -93,105 +107,51 @@ function renderResult(results) {
 
 function accordionSetup() {
   $(".accordion-content").hide();
-  $(".accordion-content").not($(this).next()).slideUp('fast');
+
   $('#accordion').find('.accordion-toggle').click(function() {
+    $('.vid-content').hide();
+    let eventName = $(event.currentTarget).find('.event-name').text();
+    getDataFromYoutube(eventName, youtubeData);
     $(this).next().slideToggle('fast');
+    $(".accordion-content").not($(this).next()).slideUp('fast');
   });
+}
+
+function youtubeData(data) {
+  console.log(data);
+  let videoPrev = data.items[0].videoId;
+  // console.log();
+  $('.vid-content').show();
+  $('.vid-content').html(`<iframe id="ytplayer" type="text/html" width="360" height="202.5" src="https://www.youtube.com/embed/fOERHGU4aF4" frameborder="0" allowfullscreen>`);
+}
+
+
+function getDataFromYoutube(item, youtubeData) {
+  console.log(item);
+  let search = {
+    url: Youtube_URL,
+    data: {
+      'maxResults': '1',
+      'part': 'snippet',
+      'key': 'AIzaSyABkh-9aed8ARl7i57TSLwn0CUYu7fvfi4',
+      'q': item,
+      'type': 'video'
+    },
+    type: 'GET',
+    success: youtubeData
+  };
+
+  $.ajax(search);
+
 }
 
 
 
 
 
-//
-// function addYoutubeResults(item) {
-//   console.log(item);
-//   for (let i = 0; i < item.length; i++) {
-//     //
-//   //
-//   // getDataFromYoutube(item, youtubeData);
-//   //
-//   // function youtubeData(data) {
-//   //   // console.log(data);
-//   //   // console.log(item)
-//   //   arr = [data, item]
-//   //   // myArray.push(arr);
-//   // }
-//
-//   }
-//   // await console.log(myArray);
-//
-// }
-
-
-
-
-
-
-// function addYoutubeResults(results) {
-//   for (i = 0; i < result.length; i++) {
-//     return
-//
-//   }
-// }
-
-
-
-
-
-
-// function youtubeData(data) {
-//   console.log(data)
-// }
-//
-// function getDataFromYoutube(item, youtubeData) {
-//   // console.log(item);
-//   let search = {
-//     url: Youtube_URL,
-//     data: {
-//       'maxResults': '1',
-//       'part': 'snippet',
-//       'key': 'AIzaSyABkh-9aed8ARl7i57TSLwn0CUYu7fvfi4',
-//       'q': `
-//     // $ {
-//     //   item.name
-//     // }
-//     `,
-//       'type': 'video'
-//     },
-//     type: 'GET',
-//     success: youtubeData
-//   };
-//
-//   $.ajax(search);
-//
-// }
-
-
-
-//
-//
-//
-
-
-//     }.Youtube Image < img src = '${updateItem.snippet.thumbnails.medium}' > < /div>
-//
-
-//   return `${item.priceRanges[0].min}& ${item.name} & ${item.dates.start.localDate} `
-//
-// }
-
-
-// getDataFromYoutube(item, youtubeData);
-//
-//
-// function youtubeData(data) {
-//   // updateItem = addYoutubeObj(item, data);
-//   console.log(data);
-// }
-//
-// function addYoutubeObj(item, data) {
-//   return $.extend(item, data.items[0]);
-
-$(watchSubmit)
-$()
+$(document).ready(function() {
+  $(watchSubmit)
+  let today = new Date().toISOString().substr(0, 10);
+  document.querySelector("#today-date").valueAsDate = new Date();
+  console.log("ready")
+});
