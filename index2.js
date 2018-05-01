eaaef0bf6a86646bd617b7bc551b265bc3e05912
George Barkin <george.barkin@gmail.com>
2018-04-30 22:18:37 -0400

    Update index2.js

--- index2.js
+++ index2.js
@@ -59,7 +59,7 @@ function collectFormData() {
     unit: 'miles',
     startDateTime: `${myDate}T00:00:00Z`,
     endDateTime: `${shortDate}T00:00:00Z`,
-    size: 100,
+    size: 75,
     page: 0
   };
   // getDataFrom Api ticketmaster
